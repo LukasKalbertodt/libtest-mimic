314590e67ef3db1b1b179fd15e992e09e9a578f8
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-07-22 14:04:54 +0200

    Add example with 100 dummy tests

---
+++ examples/many.rs
@@ -0,0 +1,31 @@
+extern crate test_cli;
+extern crate structopt;
+
+use test_cli::{Arguments, Test, TestOutcome, run_tests};
+
+
+fn main() {
+    // Parse CLI args
+    let args = Arguments::from_args();
+
+    // Generate 100 tests with dummy names
+    let tests = (0..100)
+        .map(|i| {
+            Test {
+                name: format!("test-{:03}", i),
+                kind: String::new(),
+                data: i,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // Run tests
+    run_tests(&args, &tests, |test| {
+        // We want this one test to fail
+        if test.data == 53 {
+            TestOutcome::Failed
+        } else {
+            TestOutcome::Passed
+        }
+    });
+}
