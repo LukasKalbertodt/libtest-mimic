6084b4fbd5b3459ef953a2980b620df6d4ace6f7
msrd0 <msrd0@users.noreply.github.com>
2022-11-05 17:39:33 +0100

    Update clap to version 4 (#20)
    * Update clap to 4.0.8
    
    * update deprecated derive syntax
    
    * test and fixes
    
    * Bump MSRV to 1.60
    
    * format code
    
    * Revert "format code"
    
    This reverts commit 735be0b9441f96bc024f876763f3018eab90b7f5.
    
    * find . -name '*.rs' | while read file; do sed -E -i -e 's,\t,    ,g' $file; done

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 /// Command line arguments.
 ///
@@ -12,7 +12,7 @@ use clap::Parser;
 /// official test harness. There are also some other minor CLI differences, but
 /// the main use cases should work exactly like with the built-in harness.
 #[derive(Parser, Debug, Clone, Default)]
-#[clap(
+#[command(
     help_template = "USAGE: [OPTIONS] [FILTER]\n\n{all-args}\n\n\n{after-help}",
     disable_version_flag = true,
     after_help = "By default, all tests are run in parallel. This can be altered with the \n\
@@ -21,36 +21,36 @@ use clap::Parser;
 pub struct Arguments {
     // ============== FLAGS ===================================================
     /// Run ignored and non-ignored tests.
-    #[clap(long = "--include-ignored", help = "Run ignored tests")]
+    #[arg(long = "include-ignored", help = "Run ignored tests")]
     pub include_ignored: bool,
 
     /// Run only ignored tests.
-    #[clap(long = "--ignored", help = "Run ignored tests")]
+    #[arg(long = "ignored", help = "Run ignored tests")]
     pub ignored: bool,
 
     /// Run tests, but not benchmarks.
-    #[clap(
-        long = "--test",
+    #[arg(
+        long = "test",
         conflicts_with = "bench",
         help = "Run tests and not benchmarks",
     )]
     pub test: bool,
 
     /// Run benchmarks, but not tests.
-    #[clap(long = "--bench", help = "Run benchmarks instead of tests")]
+    #[arg(long = "bench", help = "Run benchmarks instead of tests")]
     pub bench: bool,
 
     /// Only list all tests and benchmarks.
-    #[clap(long = "--list", help = "List all tests and benchmarks")]
+    #[arg(long = "list", help = "List all tests and benchmarks")]
     pub list: bool,
 
     /// No-op, ignored (libtest-mimic always runs in no-capture mode)
-    #[clap(long = "--nocapture", help = "No-op (libtest-mimic always runs in no-capture mode)")]
+    #[arg(long = "nocapture", help = "No-op (libtest-mimic always runs in no-capture mode)")]
     pub nocapture: bool,
 
     /// If set, filters are matched exactly rather than by substring.
-    #[clap(
-        long = "--exact",
+    #[arg(
+        long = "exact",
         help = "Exactly match filters rather than by substring",
     )]
     pub exact: bool,
@@ -60,9 +60,9 @@ pub struct Arguments {
     ///
     /// This is an alias for `--format=terse`. If this is set, `format` is
     /// `None`.
-    #[clap(
+    #[arg(
         short = 'q',
-        long = "--quiet",
+        long = "quiet",
         conflicts_with = "format",
         help = "Display one character per test instead of one line. Alias to --format=terse",
     )]
@@ -70,8 +70,8 @@ pub struct Arguments {
 
     // ============== OPTIONS =================================================
     /// Number of threads used for parallel testing.
-    #[clap(
-        long = "--test-threads",
+    #[arg(
+        long = "test-threads",
         help = "Number of threads used for running tests in parallel. If set to 1, \n\
             all tests are run in the main thread.",
     )]
@@ -79,8 +79,8 @@ pub struct Arguments {
 
     /// Path of the logfile. If specified, everything will be written into the
     /// file instead of stdout.
-    #[clap(
-        long = "--logfile",
+    #[arg(
+        long = "logfile",
         value_name = "PATH",
         help = "Write logs to the specified file instead of stdout",
     )]
@@ -88,18 +88,18 @@ pub struct Arguments {
 
     /// A list of filters. Tests whose names contain parts of any of these
     /// filters are skipped.
-    #[clap(
-        long = "--skip",
+    #[arg(
+        long = "skip",
         value_name = "FILTER",
-        number_of_values = 1,
+        num_args = 1,
         help = "Skip tests whose names contain FILTER (this flag can be used multiple times)",
     )]
     pub skip: Vec<String>,
 
     /// Specifies whether or not to color the output.
-    #[clap(
-        long = "--color",
-        possible_values = &["auto", "always", "never"],
+    #[arg(
+        long = "color",
+        value_enum,
         value_name = "auto|always|never",
         help = "Configure coloring of output: \n\
             - auto = colorize if stdout is a tty and tests are run on serially (default)\n\
@@ -109,9 +109,9 @@ pub struct Arguments {
     pub color: Option<ColorSetting>,
 
     /// Specifies the format of the output.
-    #[clap(
-        long = "--format",
-        possible_values = &["pretty", "terse"],
+    #[arg(
+        long = "format",
+        value_enum,
         value_name = "pretty|terse|json",
         help = "Configure formatting of output: \n\
             - pretty = Print verbose output\n\
@@ -121,8 +121,8 @@ pub struct Arguments {
 
     // ============== POSITIONAL VALUES =======================================
     /// Filter string. Only tests which contain this string are run.
-    #[clap(
-        name = "FILTER",
+    #[arg(
+        value_name = "FILTER",
         help = "The FILTER string is tested against the name of all tests, and only those tests \
                 whose names contain the filter are run.",
     )]
@@ -152,7 +152,7 @@ impl Arguments {
 }
 
 /// Possible values for the `--color` option.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
 pub enum ColorSetting {
     /// Colorize output if stdout is a tty and tests are run on serially
     /// (default).
@@ -184,7 +184,7 @@ impl FromStr for ColorSetting {
 }
 
 /// Possible values for the `--format` option.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
 pub enum FormatSetting {
     /// One line per test. Output for humans. (default)
     Pretty,
@@ -207,5 +207,16 @@ impl FromStr for FormatSetting {
             "terse" => Ok(FormatSetting::Terse),
             _ => Err("invalid output format"),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_cli() {
+        use clap::CommandFactory;
+        Arguments::command().debug_assert()
     }
 }
